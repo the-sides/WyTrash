84151e305c5e0dc646256fe0c86c7455e7dacc66
the-sides <sides798@gmail.com>
2019-03-02 15:01:07 -0500

    sample pins displayed

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,9 +7,40 @@ function initMap() {
     center: {lat: 35.9606384, lng: -83.9207392},
     zoom: 15
   });
+
+
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length]
+    });
+  });
+  var markerCluster = new MarkerClusterer(map, markers,
+    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+}
+
+var locations = [{lat:35.963733, lng:-83.917751},{lat: 35.9606384, lng: -83.9207392}];
+
+function updateMap(data){
+  console.log(data.positions)
+  // return da
+  // locations = data.positions;
+  // for(keys in data.positions){
+
+  //   console.log(data.positions[keys])
+  // }
+
+  
+  // return true;
 }
 
 $(function(){
   $('#jstatus').text("GO");
-  
+  $.get("/trip-report", function(data,status){
+    console.log("Data: ", data);
+    console.log("Status: ", status);
+    locations = data.positions;
+  })
 })
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WyTrash', truck_status: 'On Route...' });
 });
 
+// Request data from DB
+payload = {positions:[{lat:35.963733, lng:-83.917751},{lat: 35.9606384, lng: -83.9207392}]};
 router.get('/trip-report', function(req,res){
-  res.send({foo:'bar'});
+  res.send(payload);
 })
 
 module.exports = router;
