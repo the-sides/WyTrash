7703874de2a2a4030e03d6a91439cf6b77dca099
the-sides <sides798@gmail.com>
2019-03-02 16:08:09 -0500

    pin update

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,46 +1,69 @@
 console.log("Welcome to client-side script")
 
 // GOOGLE MAPS
-var map;
+var map, locations;
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 35.9606384, lng: -83.9207392},
     zoom: 15
   });
+}
 
 
-  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var markers = locations.map(function(location, i) {
+function updateMap(data){
+  var markers = data.positions.map(function(location, i) {
     return new google.maps.Marker({
       position: location,
       label: labels[i % labels.length]
     });
   });
-  var markerCluster = new MarkerClusterer(map, markers,
-    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
+  for(i in markers)
+    markers[i].setMap(map);
 }
 
-var locations = [{lat:35.963733, lng:-83.917751},{lat: 35.9606384, lng: -83.9207392}];
-
-function updateMap(data){
-  console.log(data.positions)
-  // return da
-  // locations = data.positions;
-  // for(keys in data.positions){
-
-  //   console.log(data.positions[keys])
-  // }
-
-  
-  // return true;
-}
 
 $(function(){
   $('#jstatus').text("GO");
   $.get("/trip-report", function(data,status){
     console.log("Data: ", data);
     console.log("Status: ", status);
-    locations = data.positions;
+    // locations = data.positions;
+    updateMap(data);
   })
 })
+
+
+
+  // Pre-loaded cluster
+  // var markers = locations.map(function(location, i) {
+  //   return new google.maps.Marker({
+  //     position: location,
+  //     label: labels[i % labels.length]
+  //   });
+  // });
+  // var markerCluster = new MarkerClusterer(map, markers,
+  //   {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+  
+  // <!-- <script>
+  //   var myLatlng = new google.maps.LatLng(35,-84);
+  //   var mapOptions = {
+  //     zoom: 4,
+  //     center: myLatlng
+  //   }
+  //   // I'm using 
+  //   // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  //   var marker = new google.maps.Marker({
+  //       position: myLatlng,
+  //       title:"Hello World!"
+  //   });
+  //   // To add the marker to the map, call setMap();
+  //   // map is a global variable defined in client.js
+  //   marker.setMap(map);
+  // </script> -->
+
+  // <!-- Map Initial Load -->
+  // <!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2BkiI59erV23pjx7bfHwvVniaqQFyVLg&callback=initMap" ></script> -->
+  // <!-- Marker Clusters -->
+  // <!-- <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script> -->
