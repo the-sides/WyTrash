0e22a18609287eedfdf456bf12bf40182820d8f7
the-sides <sides798@gmail.com>
2019-03-02 13:50:40 -0500

    knoxville map and jquery

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,1 +1,15 @@
-console.log("Welcome to client side script")
+console.log("Welcome to client-side script")
+
+// GOOGLE MAPS
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 35.9606384, lng: -83.9207392},
+    zoom: 15
+  });
+}
+
+$(function(){
+  $('#jstatus').text("GO");
+  
+})
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'WyTrash', truck_status: 'On Route...' });
 });
 
+router.get('/trip-report', function(req,res){
+  res.send({foo:'bar'});
+})
+
 module.exports = router;
