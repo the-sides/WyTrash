62ecf4868ca982cd9ed163d916da3b87ef40bd2f
the-sides <sides798@gmail.com>
2019-03-02 20:44:58 -0500

    favicons and friends

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var favicon = require('express-favicon');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
